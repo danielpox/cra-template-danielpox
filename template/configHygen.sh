74412f9f07d182d374f8a90572e10e17e40fef7c
Daniel Hallgren <1824229+danielpox@users.noreply.github.com>
2019-12-14 01:32:46 +0000

    Add error check of env variable to configHygen script

--- template/configHygen.sh
+++ template/configHygen.sh
@@ -7,4 +7,9 @@ then
   export $(cat .env.local | sed 's/#.*//g' | xargs)
 fi
 
-git clone https://$GITHUB_PERSONAL_TOKEN:x-oauth-basic@github.com/danielpox/hygen-danielpox.git && mv hygen-danielpox/_templates . && rm -rf hygen-danielpox
+if [ ! -z "$GITHUB_PERSONAL_TOKEN"]
+then
+  git clone https://$GITHUB_PERSONAL_TOKEN:x-oauth-basic@github.com/danielpox/hygen-danielpox.git && mv hygen-danielpox/_templates . && rm -rf hygen-danielpox
+else
+  echo 'You must provide a GitHub Personal Token. Have you copied .env.example to .env.local and made changes?'
+fi
