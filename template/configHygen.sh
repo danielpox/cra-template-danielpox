10716d616f8b7f79fc099b9a61aab22530099275
Daniel Hallgren <1824229+danielpox@users.noreply.github.com>
2019-12-14 01:05:28 +0000

    Fix bash script env file name error

--- template/configHygen.sh
+++ template/configHygen.sh
@@ -4,7 +4,7 @@ set -e
 
 if [ -f .env.local ]
 then
-  export $(cat .env | sed 's/#.*//g' | xargs)
+  export $(cat .env.local | sed 's/#.*//g' | xargs)
 fi
 
 git clone https://$GITHUB_PERSONAL_TOKEN:x-oauth-basic@github.com/danielpox/hygen-danielpox.git && mv hygen-danielpox/_templates . && rm -rf hygen-danielpox
